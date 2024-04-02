a3b943a9a69e4a28b2b2dcfb443c4986c24cb0e1
Joot111 <joaoamoroso04@gmail.com>
2024-04-02 10:03:33 +0100

    Tabelas

--- Aulas/Aulas/Data/ApplicationDbContext.cs
+++ Aulas/Aulas/Data/ApplicationDbContext.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Aulas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aulas.Data
 {
+    /// <summary>
+    /// classe responsável pela criação e gestão de Base de Dados
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext
     {
+        /// <param name="options"></param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
+            : base(options){
         }
+
+        // definição das 'tabelas'
+        public DbSet <Utilizadores> Utilizadores { get; set; }
+
+        public DbSet <Alunos>  Alunos { get; set; }
+
+        public DbSet <Professores> Professores { get; set; }
+
+        public DbSet <Cursos> Cursos { get; set; }
+
+        public DbSet <UnidadesCurriculares> UCs { get; set; }
+
+        public DbSet <Inscricoes> Inscricoes { get; set; }
     }
 }
